b6758e6d4339915d61e3be5cb24dd688bf156bf2
naqibam <naqibmrz@gmail.com>
2024-08-12 18:06:04 +0800

    model

--- FlaskProject/gunicorn_config.py
+++ FlaskProject/gunicorn_config.py
@@ -1,2 +1,3 @@
-bind = "0.0.0.0:8080"
-workers = 2
+version https://git-lfs.github.com/spec/v1
+oid sha256:f191a06bd8066c87abbcdc8241f470e02ed2e212bf66301bf7fedf3ee2482b2c
+size 33
--- FlaskProject/test.py
+++ FlaskProject/test.py
@@ -1,28 +1,3 @@
-from flask import Flask, jsonify, request,render_template,redirect, url_for,session, flash
-from flask_sqlalchemy import SQLAlchemy
-
-import urllib.parse
-import pypyodbc as odbc
-
-from credentials import username, password
-
-
-app = Flask(__name__)
-
-app.secret_key = 'supersecretkey'
-
-
-params = urllib.parse.quote_plus("Driver={ODBC Driver 18 for SQL Server};Server=tcp:aap-221734q.database.windows.net,1433;Database=aap;Uid=aap-user;Pwd=pM3sEHAZ+_2hu5-;Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;")
-app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=" + params
-#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://aap:mysql@localhost:3306/aap'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
- 
-    app.run(debug=True)
+version https://git-lfs.github.com/spec/v1
+oid sha256:f306bc1966e9cea6809d7fd0f2ebd7e6ae7af71ca2884e1523466841e64df593
+size 908
